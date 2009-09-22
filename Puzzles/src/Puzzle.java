bc16162fbb2c7941ea23d6a3ec87a57280a2cc86
torbjoern.k <torbjoern.k@82e4057e-a689-11de-be8d-735280eae207>
2009-09-22 05:19:20 +0000

    Nabil and Scott sended me some code and annotations. I implemented them as good as I could.

--- Puzzles/src/Puzzle.java
+++ Puzzles/src/Puzzle.java
@@ -3,38 +3,90 @@ import java.io.IOException;
 public class Puzzle {
 	private int pieceCount;
 	private Piece[] pieces;
-	
-	public Puzzle(int pieceCount) throws IOException {
-		this.pieceCount = pieceCount;
-		this.pieces = new Piece[pieceCount];
-		for(int i = 0; i < pieceCount; i++) {
-			setPiece(i);
-		}
-	}
-	
-	public int[] solve() {
-		int[] sln = new int[pieceCount];
-		//TODO algorithm
-		return sln;
-	}
-	
-	public void setPiece(int id) throws IOException {
-		this.pieces[id] = new Piece(id, InOut.getPieceShapeFromUser(id));
-	}
-	
-	public Piece[] getPieces() {
-		return this.pieces;
-	}
-	
-	public Piece getPiece(int id) {
-		return this.pieces[id];
-	}
-	
-	public int getPieceCount() {
-		return this.pieceCount;
-	}
-	
-	public void printSln(int[] sln) {
-		//TODO print out
-	}
+   
+ 	public Puzzle(int pieceCount) throws IOException {
+ 		this.pieceCount = pieceCount;
+	 	this.pieces = new Piece[pieceCount];
+	 	for(int i = 0; i < pieceCount; i++) {
+	 		setPiece(i);
+	 	}
+ 	}
+    
+ 	/**
+ 	 * the algorithm
+ 	 * -implemented by Tobbe-
+ 	 * @return
+ 	 */
+ 	public int[] solve() {
+ 		int[] sln = new int[pieceCount];
+ 		placePiece(this, 1);
+ 		//TODO algorithm
+ 		return sln;
+ 	}
+ 	
+ 	/**
+ 	 * I'm not sure about the usage (Tobbe)
+ 	 * -implemented by Nabil-
+ 	 * @param side
+ 	 * @return
+ 	 */
+ 	public int getMatchingSideCount(Side side) {
+ 		int count = 0;
+ 		for(Piece piece : pieces) {
+ 			count += piece.getMatchingSides(side);
+ 		}
+ 		return count;
+ 	}
+        
+ 	public void setPiece(int id) throws IOException {
+ 		this.pieces[id] = new Piece(id, InOut.getPieceShapeFromUser(id));
+ 	}
+        
+ 	public Piece[] getPieces() {
+ 		return this.pieces;
+ 	}
+        
+ 	public Piece getPiece(int id) {
+ 		return this.pieces[id];
+ 	}
+        
+ 	public int getPieceCount() {
+ 		return this.pieceCount;
+ 	}
+        
+ 	public void printSln(int[] sln) {
+ 		//TODO print out
+ 	}
+ 	
+ 	/** Places a piece then returns the id of all the pieces in the puzzle 
+ 	 *  that can be matched to that piece.
+ 	 */
+ 	public int[] placePiece(Puzzle a, int index){
+ 		int[] matches;
+ 		int count =0;
+ 		Piece p = a.getPiece(index);
+ 		if(p.getPieceShape()) {
+ 			matches = new int[4];
+ 		}
+ 		else {
+ 			matches = new int[3];
+ 		}
+ 		for(int i =1; i<=14; i++){
+ 			// TODO: review this for-loop and its if-checks
+ 			if(i == index){ // ???
+ 			}
+ 			else { // ???
+ 				if(p.hasMatch(a.getPiece(i))) { // ???
+ 					matches[i] = i;
+ 					count++;
+ 				}
+ 			}
+ 		}
+ 		//couldn't match this piece to enough other pieces
+ 		if(count < matches.length) {
+ 			System.out.println("This puzzle is unsolvable");
+        	//perhaps some sort of exception to be thrown?
+ 		}
+ 		return matches;
+ 	}
 }
--- Puzzles/src/Side.java
+++ Puzzles/src/Side.java
@@ -1,19 +1,28 @@
-
 public class Side {
 	private boolean shape;
 	private int colour;
 	private boolean orientation;
 	
 	public Side() {
+		this.shape = true;
+		this.colour = 0;
+		this.orientation = true;
 	}
 	
+	/**
+	 * probably we do not need this constructor
+	 * may be deleted in later versions
+	 */
 	public Side(boolean shape, int colour, boolean orientation) {
 		this.shape = shape;
 		this.colour = colour;
 		this.orientation = orientation;
 	}
 	
-	// two sides match if colour is same and shape and orientation is flipped
+	/**
+	 * the equals-function (from the other point of view)
+	 * two sides match if colour is same and shape and orientation is vice versa
+	 */
 	public boolean match(Side side) {
 		if((this.colour == side.getColour())
 				&& (this.shape != side.getShape())
@@ -26,18 +35,23 @@ public class Side {
 	public boolean getShape() {
 		return shape;
 	}
+	
 	public void setShape(boolean shape) {
 		this.shape = shape;
 	}
+	
 	public int getColour() {
 		return colour;
 	}
+	
 	public void setColour(int colour) {
 		this.colour = colour;
 	}
+	
 	public boolean getOrientation() {
 		return orientation;
 	}
+	
 	public void setOrientation(boolean orientation) {
 		this.orientation = orientation;
 	}
